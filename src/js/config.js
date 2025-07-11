d50405faf0e7ec9a62ce2327c613cb9e46e1ac75
Rahoz2 <rahozumed96@gmail.com>
2025-07-11 18:58:19 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7623ffe8-0ca2-460d-8095-044796c9d7d4';
+export const MODAL_CLOSE_SEC = 2.5;
